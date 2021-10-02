e57fddd9b8313fb48f5968ce30c8e5bd097c21e4
Gourav Saini <mailtogouravsaini@gmail.com>
2021-10-02 14:35:40 +0530

    option to choose theme

--- python_based_timer.py
+++ python_based_timer.py
@@ -1,5 +1,5 @@
 from random import randrange
-from tkinter import Label, Tk
+from tkinter import Label, Tk, StringVar, OptionMenu
 import time
 
 app_window = Tk() 
@@ -23,16 +23,24 @@ colors = [
     ("red", "blue"),
 ]
 
-background, foreground = colors[randrange(len(colors))]
+options_list = ["Dark", "Dark Inverse", "B&W", "B&W Inverse", "Blue", "Blue Inverse", "Apocalypse", "Apocalypse Inverse", "Struggle", "Struggle Inverse", "Juggler", "Juggler Inverse", "Pluto", "Pluto Inverse"]
+
+selection = StringVar(app_window)
+selection.set("Dark")
+question_menu = OptionMenu(app_window, selection, *options_list)
+question_menu.grid(row=0, column=1)
+
+background, foreground = colors[0]
 text_font= ("Boulder", 68, 'bold')
 border_width = 25
 
-label = Label(app_window, font=text_font, bg=background, fg=foreground, bd=border_width) 
-label.grid(row=0, column=1)
+label = Label(app_window, font=text_font, bg=background, fg=foreground, bd=border_width)
+label.grid(row=1, column=1)
 
 def digital_clock(): 
    time_live = time.strftime("%H:%M:%S")
-   label.config(text=time_live) 
+   background, foreground = colors[options_list.index(selection.get())]
+   label.config(text=time_live, bg=background, fg=foreground)
    label.after(200, digital_clock)
 
 digital_clock()
